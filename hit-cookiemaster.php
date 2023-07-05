e95e22ecf3276eb94e3a75efc989b21ea06c363d
kjell-heinsen <60947352+kjell-heinsen@users.noreply.github.com>
2023-07-05 09:13:51 +0200

    Change: Update-Struktur

--- hit-cookiemaster.php
+++ hit-cookiemaster.php
@@ -8,45 +8,34 @@ Version: 0.0.0
 Author URI: https://kjell-heinsen.de/
 */
 
-define( 'HITCOOKIEMASTER_VERSION', '0.0.0' );
-define( 'HITCOOKIEMASTER_MIN_PHP',   '7.4.0' );
-define( 'HITCOOKIEMASTER_MIN_WP',    '6.0.0' );
-define( 'HITCOOKIEMASTER_TESTED_WP',    '6.2.2' );
-define( 'HITCOOKIEMASTER_DIRPATH', plugin_dir_path( __FILE__ ) );
-define( 'HITCOOKIEMASTER_LIB', plugin_dir_path( __FILE__ ) . 'lib/' );
-define( 'HITCOOKIEMASTER_CLASSES', plugin_dir_path( __FILE__ ) . 'classes/' );
+
+require_once  'autoload.php';
+require_once 'config.php';
+$basename = 'hit-cookiemaster';
+$project_id = '';
 
 
 
 
-function hit_cookiemaster_autoloader($class)
+
+
+if(DEBUG)
 {
-    $myfilename = HITCOOKIEMASTER_CLASSES . strtolower($class) . ".php";
-
-    if (file_exists($myfilename))
-    {
-        require_once $myfilename;
-    }
-
-    $myfilename = HITCOOKIEMASTER_LIB . strtolower($class) . ".php";
-
-    if (file_exists($myfilename))
-    {
-        require_once $myfilename;
-    }
-
+    $plugin_updateurl = "https://dev.wpu.heinsen-it.de/";
+}
+else
+{
+    $plugin_updateurl = "https://wpu.heinsen-it.de/";
 }
 
-
-spl_autoload_register("hit_cookiemaster_autoloader");
-
-
-
+$myupdateoptions = new HIT_COOKIEMASTER_CreateUpdateOption();
+$plugin_updateoptions = $myupdateoptions->init();
 
 
 require HITCOOKIEMASTER_LIB.'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://wpu.heinsen-it.de/updates/hit-cookiemaster/info.json',
-    __FILE__, //Full path to the main plugin file or functions.php.
-    'hit-cookiemaster'
+$MyUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    $plugin_updateurl.'updates/'.$basename.'/'.$project_id.'/'.$plugin_updateoptions,
+    $plugin_updateurl.'downloads/'.$basename.'/', //Metadata URL.
+    __FILE__, //Full path to the main plugin file.
+    $basename //Plugin slug. Usually it's the same as the name of the directory.
 );
