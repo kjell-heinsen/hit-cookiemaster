45c7d6b49b1fbaac7fe949ea59dbcf1f427f21ee
kjell-heinsen <60947352+kjell-heinsen@users.noreply.github.com>
2023-11-11 01:07:37 +0100

    New Plugin Structure

---
+++ hit-cookiemaster/classes/hit_cookiemaster_adminmenu.php
@@ -0,0 +1,53 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No Time for Looking for Freedom' );
+if(!class_exists('HIT_COOKIEMASTER_AdminMenu')) {
+	class HIT_COOKIEMASTER_AdminMenu {
+
+
+		public static function add_cookiemaster_menu() {
+
+			if ( ! HIT_COOKIEMASTER_AdminMenu::ItemExists( 'hit_overview', false ) ) {
+				add_menu_page( 'HIT Plugins', 'HIT Plugins', 'manage_options', 'hit_overview', 'HIT_COOKIEMASTER_Sites::startseite', 'dashicons-plugins-checked', 60000 );
+			}
+			if ( ! HIT_COOKIEMASTER_AdminMenu::ItemExists( 'hit_overview', true ) ) {
+				add_submenu_page(
+					'hit_overview',               // parent slug
+					'Cookiemaster',                      // page title
+					'Cookiemaster',                      // menu title
+					'manage_options',                   // capability
+					'hit_overview',               // slug
+					'HIT_SUPPORT_Sites::cookiemaster' // Funktionsname
+				);
+			}
+		}
+
+		public static function CleanupMenu()
+		{
+			remove_submenu_page( 'hit_overview');
+		}
+
+		private static function ItemExists( $handle, $sub = false ) {
+			global $menu, $submenu;
+			$check_menu = $sub ? $submenu : $menu;
+			if ( empty( $check_menu ) ) {
+				return false;
+			}
+			foreach ( $check_menu as $k => $item ) {
+				if ( $sub ) {
+					foreach ( $item as $sm ) {
+						if ( $handle == $sm[2] ) {
+							return true;
+						}
+					}
+				} else {
+					if ( $handle == $item[2] ) {
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
+	}
+}
