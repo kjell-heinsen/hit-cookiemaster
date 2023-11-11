33d4fd9b10dd52e1f8df186e90f87fe5fd31b94a
kjell-heinsen <60947352+kjell-heinsen@users.noreply.github.com>
2023-11-11 01:07:37 +0100

    New Plugin Structure

---
+++ hit-cookiemaster/js/blockcookie.js
@@ -0,0 +1,16 @@
+// Blockiert das Setzen von Cookies
+Object.defineProperty(document, 'cookie', {
+    get: function() {
+        return '';
+    },
+    set: function() {
+        return false;
+    }
+});
+
+// Löscht alle vorhandenen Cookies
+document.cookie.split(';').forEach(function(cookie) {
+    var eqPos = cookie.indexOf('=');
+    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+});
