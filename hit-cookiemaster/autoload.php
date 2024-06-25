587ee575bcb953d0ea4f2c44478010f8334c3c46
kjell-heinsen <60947352+kjell-heinsen@users.noreply.github.com>
2024-06-25 23:32:01 +0200

    Rerange Files

--- hit-cookiemaster/autoload.php
+++ hit-cookiemaster/autoload.php
@@ -1,23 +1,38 @@
 <?php
 
 
-function hit_cookiemaster_autoloader($class)
-{
-    $myfilename = HITCOOKIEMASTER_CLASSES . strtolower($class) . ".php";
+spl_autoload_register(function ($class) {
 
-    if (file_exists($myfilename))
-    {
-        require_once $myfilename;
+    // project-specific namespace prefix
+    $prefix = 'hitcodeblockelementor\\';
+   // $prefix = '';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
     }
 
-    $myfilename = HITCOOKIEMASTER_LIB . strtolower($class) . ".php";
+    // get the relative class name
+    $relative_class = substr($class, $len);
 
-    if (file_exists($myfilename))
-    {
-        require_once $myfilename;
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require_once $file;
     }
 
-}
-
-
-spl_autoload_register("hit_cookiemaster_autoloader");
+    $file = $base_dir . str_replace('\\', '/', strtolower($relative_class)) . '.php';
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
