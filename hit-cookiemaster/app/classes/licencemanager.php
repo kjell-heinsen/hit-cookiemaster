aa12a97392c14376657295b851d680ff018e44ff
kjell-heinsen <60947352+kjell-heinsen@users.noreply.github.com>
2024-07-02 00:42:40 +0200

    licencemanager

--- hit-cookiemaster/app/classes/licencemanager.php
+++ hit-cookiemaster/app/classes/licencemanager.php
@@ -13,14 +13,8 @@ defined('ABSPATH') or die('No Time for Looking for Freedom');
             if (in_array($cipher, openssl_get_cipher_methods())) {
              $return =   openssl_encrypt(self::GetSiteID().'-xxxx',$cipher,self::GetProjectID(),0,self::GetProjectID());
             }
-
             return $return;
-            /*
-            $datasrv = HITWPLIB_DIRPATH;
-            $url = $datasrv . 'p.id';
-            $ID = file_get_contents($url);
-            return $ID; */
-        }
+           }
 
 
         public static function Verify():bool
@@ -32,7 +26,7 @@ defined('ABSPATH') or die('No Time for Looking for Freedom');
 
 
         public static function GetSiteID():string{
-            $datasrv = HIT_CODEBLOCK_ELEMENTOR_DIRPATH;
+            $datasrv = HITCOOKIEMASTER_DIRPATH;
             $url = $datasrv . 'site.id';
             $ID = file_get_contents($url);
             return $ID;
@@ -40,7 +34,7 @@ defined('ABSPATH') or die('No Time for Looking for Freedom');
 
         public static function GetProjectID():string
         {
-            $datasrv = HIT_CODEBLOCK_ELEMENTOR_DIRPATH;
+            $datasrv = HITCOOKIEMASTER_DIRPATH;
             $url = $datasrv . 'p.id';
             $ID = file_get_contents($url);
             return $ID;
